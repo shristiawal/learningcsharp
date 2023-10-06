c92d88fbfd1f671e21ce2061a2924c77fd6c0daf
shristiawal <102451898+shristiawal@users.noreply.github.com>
2023-10-06 23:15:38 +0545

    palindrome

--- learningcsharp/Program.cs
+++ learningcsharp/Program.cs
@@ -9,7 +9,34 @@ namespace learningcsharp
     internal class Program
     {
         static void Main(string[] args)
+
         {
+            int  reverse = 0, remainder;
+           
+
+            Console.WriteLine("enter the number");
+            int n = int.Parse(Console.ReadLine());
+            int num = n;
+            while (n != 0)
+            {
+                remainder = n % 10;
+                reverse = reverse * 10 + remainder;
+                n /= 10;
+           
+            }
+           
+            if (num == reverse)
+            {
+                Console.WriteLine("number is palindrome");
+                Console.ReadLine();
+
+            }
+            else
+            {
+                Console.WriteLine("number is not palindrome");
+                Console.ReadLine();
+            }
+            
         }
     }
 }
